3b80a9d4ac7d16511ecdcc8f5a47b8e20df579b6
mairon <maironcontatos@hotmail.com>
2016-05-20 00:14:49 -0300

    ajust atlas site novo

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -101,57 +101,71 @@ class Crawler < ActiveRecord::Base
 
 				mechanize = Mechanize.new
 				page = mechanize.get('http://atlascambio.tur.br/')
-				get_val = page.search(".//strong")
+				get_val = page.search(".//span")
 
-			  us_c = ''
-			  us_v = ''
-			  ps_c = ''
-			  ps_v = ''
-			  es_c = ''
-			  es_v = ''
-			  gs_c = ''
-			  gs_v = ''
+
+				us_rs_c = ''
+				us_rs_v = ''
+
+				rs_gs_c = ''
+				rs_gs_v = ''
+
+				ps_rs_c = ''
+				ps_rs_v = ''
+
+				es_rs_c = ''
+				es_rs_v = ''
 			  count = 0
 			  content = ''
-		 	get_val.each do |c| 
-			 if c.to_s.gsub(/\s+/, "") != '<strong>Menu</strong>'.to_s.gsub(/\s+/, "")
-			
-				content = c.to_s.encode('UTF-8').gsub(/\s+/, "").gsub(160.chr("UTF-8"),"").gsub('<strongvalue="',"").gsub('">', "").gsub('</strong>', "").gsub(',','.')
 
-				if content[0..15] == 'DolarxRealCompra' 
-					us_c = content[16..19] 
-				end 
-				if content[0..14] == 'RealVendaxDolar' 
-					us_v = content[15..18] 
-				end 
-				if content[0..14] == 'EuroxRealCompra' 
-					es_c = content[15..18] 
-				end 
-				if content[0..13] == 'RealVendaxEuro' 
-					es_v = content[14..17] 
-				end 
-				if content[0..14] == 'PesoxRealCompra' 
-					ps_c = content[15..18] 
-				end 
-				if content[0..13] == 'PesoxRealVenda' 
-					ps_v = content[14..17] 
-				end 
-				if content[0..17] == 'GuaranixRealCompra' 
-					gs_c = content[18..21] 
-				end 
-				if content[0..16] == 'GuaranixRealVenda' 
-					gs_v = content[17..20] 
-				end
-			 end 
-		 	end 
+				 get_val.each do |c| 
+					 if c.to_s.encode('UTF-8').gsub(/\s+/, "")[0..14] == '<spanclass="c4"' 
+						content =  "{#{count}}" + c.to_s.encode('UTF-8').gsub(/\s+/, "").gsub(".","").gsub(",",".").gsub(160.chr("UTF-8"),"").gsub('<spanclass="c4">',"")
+
+						 if content[0..2] == '{0}' 
+							 us_rs_c = content[3..6] 
+						 end 
+
+						 if content[0..2] == '{1}' 
+							 us_rs_v =content[3..6] 
+						 end 
+
+						 if content[0..2] == '{2}' 
+							 ps_rs_c =content[3..6] 
+						 end 
+
+						 if content[0..2] == '{3}' 
+							 ps_rs_v =content[3..6] 
+						 end 
+
+
+						 if content[0..2] == '{4}' 
+							 es_rs_c =content[3..6] 
+						 end 
+
+						 if content[0..2] == '{5}' 
+							 es_rs_v =content[3..6] 
+						 end 
+
+						 if content[0..2] == '{6}' 
+							 rs_gs_c =content[3..6] 
+						 end 
+
+						 if content[0..2] == '{7}' 
+							 rs_gs_v =content[3..6] 
+						 end 
+					 	 count += 1 
+					  end 
+				 end 
+
 			 	#insert realxdolar
 		    Exchange.create(
 		    	company_id: 2,
 		    	date:    Time.now,
 		    	currency_id: 3,
 		    	:for =>      1,
-		    	buy:        us_c.to_f,
-		    	sale:       us_v.to_f,
+		    	buy:        us_rs_c.to_f,
+		    	sale:       us_rs_v.to_f,
 		    )
 
 			 	#insert realxpeso
@@ -160,8 +174,8 @@ class Crawler < ActiveRecord::Base
 		    	date:    Time.now,
 		    	currency_id: 3,
 		    	:for =>      4,
-		    	buy:        ps_c.to_f,
-		    	sale:       ps_v.to_f,
+		    	buy:        ps_rs_c.to_f,
+		    	sale:       ps_rs_v.to_f,
 		    )
 
 			 	#insert realxeuro
@@ -170,8 +184,8 @@ class Crawler < ActiveRecord::Base
 		    	date:    Time.now,
 		    	currency_id: 3,
 		    	:for =>      5,
-		    	buy:        es_c.to_f,
-		    	sale:       es_v.to_f,
+		    	buy:        es_rs_c.to_f,
+		    	sale:       es_rs_v.to_f,
 		    )
 			
 			 	#insert realxguarani
@@ -180,8 +194,8 @@ class Crawler < ActiveRecord::Base
 		    	date:    Time.now,
 		    	currency_id: 3,
 		    	:for =>      2,
-		    	buy:        gs_c.to_f,
-		    	sale:       gs_v.to_f,
+		    	buy:        rs_gs_c.to_f,
+		    	sale:       rs_gs_v.to_f,
 		    )
 			rescue
 				c.update_attributes(status: false)
